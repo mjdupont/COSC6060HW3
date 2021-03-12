33a31de56fd58d0f72cc9f22cc57a412f57e59a5
Matthew Dupont <matthewdupont11@gmail.com>
2021-03-12 06:44:36 -0600

    Adding more data

--- evaluate_totals_detailed.sh
+++ evaluate_totals_detailed.sh
@@ -4,14 +4,14 @@ mpicc -o DLBPolyEvaluation.exe DLBPolyEvaluation.c
 
 rm -r -f "results_detailed"
 mkdir "results_detailed"
-for file in results/queue.csv; do
+for file in results_detailed/queue.csv; do
   echo "Procs,Terms,ChunkSize,Variable,Total,MaxTime" > $file
 done
 
 for polysize in 1000; do
   for np in 1 2 3 5 9 13 16; do
     for chunkSize in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 50 100; do
-      mpirun -np $np DLBPolyEvaluation.exe -t -p $polysize -c $chunkSize >> results/queue.csv
+      mpirun -np $np DLBPolyEvaluation.exe -t -p $polysize -c $chunkSize >> results_detailed/queue.csv
       echo "Completed $polysize terms with $np procs and $chunkSize chunkSize"
     done
   done
