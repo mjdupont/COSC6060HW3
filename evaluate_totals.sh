4b6fe7ad0a1f5c1256cfe99d88b75631b0debb7f
Matthew Dupont <matthewdupont11@gmail.com>
2021-03-12 05:35:44 -0600

    New results, fixed bug with initialization

--- evaluate_totals.sh
+++ evaluate_totals.sh
@@ -8,9 +8,9 @@ for file in results/queue.csv; do
   echo "Procs,Terms,ChunkSize,Variable,Total,MaxTime" > $file
 done
 
-for polysize in 1000 10000 50000; do
+for polysize in 10 100 1000 10000 50000; do
   for np in 1 2 4 8 16; do
-    for chunkSize in 1 2 4 8 10 16 20 32; do
+    for chunkSize in 1 5 10 20 50 100; do
       mpirun -np $np DLBPolyEvaluation.exe -t -p $polysize -c $chunkSize >> results/queue.csv
       echo "Completed $polysize terms with $np procs and $chunkSize chunkSize"
     done
